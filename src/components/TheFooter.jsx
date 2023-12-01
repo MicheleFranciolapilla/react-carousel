90fedfe696b932e1777b4e001640a21cf0eba679
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-12-01 22:19:01 +0100

    Aggiustamenti in header e footer

--- src/components/TheFooter.jsx
+++ src/components/TheFooter.jsx
@@ -7,19 +7,19 @@ export default function TheFooter()
     return(
         <footer className={style.footer}>
             <div className={style.SVGContainer}>
-                <RoundedThumb filePath="./logos/react.svg" styleClasses="h-2/3 aspect-square rounded-full p-1 bg-gray-700 border-2" border-gray-800 />
                 <div className={style.textContainer}>
                     <span>Developed</span>
                     <span>with React</span>
                 </div>
+                <RoundedThumb filePath="./logos/react.svg" styleClasses="h-2/3 aspect-square rounded-full p-1 bg-gray-700 border-2" border-gray-800 />
             </div>
             <span>Dylan Dog..... un fumetto che ho amato e collezionato per tutta l'adolescenza e oltre</span>
             <div className={style.SVGContainer}>
+                <RoundedThumb filePath="./logos/vite.svg" styleClasses="h-2/3 aspect-square rounded-full p-1 bg-gray-700 border-2" border-gray-800 />
                 <div className={style.textContainer}>
                     <span>Powered</span>
                     <span>by Vite</span>
                 </div>
-                <RoundedThumb filePath="./logos/vite.svg" styleClasses="h-2/3 aspect-square rounded-full p-1 bg-gray-700 border-2" border-gray-800 />
             </div>
         </footer>
     );
