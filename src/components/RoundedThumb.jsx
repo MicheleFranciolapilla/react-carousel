0063fe91b2b934df5a25f56c35c9384477056b5d
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-12-01 16:39:28 +0100

    Componente TheHeader con sotto-componenti: OK

--- src/components/RoundedThumb.jsx
+++ src/components/RoundedThumb.jsx
@@ -1,8 +1,10 @@
+import style from "../assets/style/modules/RoundedThumbs.module.css";
+
 export default function RoundedThumb({filePath, styleClasses})
 {
     return (
-        <div className={styleClasses}>
-            <img src={filePath} alt="Immagine da visualizzare" />
+        <div className={style.thumbContainer}>
+            <img src={filePath} alt="Immagine da visualizzare" className={style.thumb} />
         </div>
     );
 }
--- src/components/TheHeader.jsx
+++ src/components/TheHeader.jsx
@@ -2,12 +2,13 @@ import style from "../assets/style/modules/TheHeaderStyle.module.css";
 
 import RoundedThumb from "./RoundedThumb";
 
-export default function TheHeader({ pageTitle })
+export default function TheHeader()
 {
     return(
         <header className={style.header}>
-            <RoundedThumb svgName="" />
-            <h1>{ pageTitle }</h1>
+            <RoundedThumb filePath="./images/groucho-700-525.jpg" />
+            <img src="./images/Dylan-Dog-Vector.svg-.png" alt="Titolo-Dylan-Dog" className={style.image} />
+            <RoundedThumb filePath="./images/dylan-610-400.jpg" />
         </header>
     );
 }
