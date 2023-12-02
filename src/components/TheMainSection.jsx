e4c1494bcf43d418eb805262838a1a2fe9e8efd4
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-12-02 12:32:08 +0100

    Classi custom Tailwind definite in oggetto

--- src/components/TheMainSection.jsx
+++ src/components/TheMainSection.jsx
@@ -1,28 +1,42 @@
 import style from "../assets/style/modules/TheMainSectionStyle.module.css";
 import albums from "../../public/files/titlesWithData.json";
 
+const percentageAndTWClasses =  {
+                                    theMainSectionHeight    :   {
+                                                                    perc    :   80,
+                                                                    TWClass :   "h-[80%]"
+                                                                },
+                                    collectionWidth         :   {
+                                                                    perc    :   90,
+                                                                    TWClass :   "w-[90%]"
+                                                                },
+                                    controlsWidth           :   {
+                                                                    perc    :   3,
+                                                                    TWClass :   "w-[3%]"
+                                                                } 
+                                }
+
 export default function TheMainSection()
 {
     const totalAlbums = albums.length;
-    const albumCollectionHeightPerc = 80;
-    const tailwindHeightClass = `h-[${albumCollectionHeightPerc}%]`;
-    const albumCollectionHeightPX = window.innerHeight * albumCollectionHeightPerc / 100;
+
+    // const theMainSectionHeightPX = window.innerHeight * theMainSectionHeightPerc / 100;
 
     return (
-        <main className={`${style.main} ${tailwindHeightClass}`}>
-            <div id="controlPrev" className={style.controls}>
+        <main className={`${style.main} ${percentageAndTWClasses.theMainSectionHeight.TWClass}`}>
+            <div id="controlPrev" className={`${style.controls} ${percentageAndTWClasses.controlsWidth.TWClass}`}>
                 <h1>P</h1>
             </div>
-            <div id="albumCollection" className={style.collection}>
+            <div id="albumCollection" className={`${style.collection} ${percentageAndTWClasses.collectionWidth.TWClass}`}>
                 {
                     albums.map( album =>
                         {
-                            console.log(album.title);
+
                         })
                 }
                 <img src="./images/albums/DYD-40.jpg" alt="Vignetta nr 4" className={style.album} />
             </div>
-            <div id="controlNext" className={style.controls}>
+            <div id="controlNext" className={`${style.controls} ${percentageAndTWClasses.controlsWidth.TWClass}`}>
                 <h1>N</h1>
             </div>
         </main>
