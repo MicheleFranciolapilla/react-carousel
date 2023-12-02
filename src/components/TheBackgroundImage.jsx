5e59067fafe8b5bf06c181130a79e041188695e3
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-12-02 10:32:37 +0100

    Rivisitazione background

--- src/components/TheBackgroundImage.jsx
+++ src/components/TheBackgroundImage.jsx
@@ -4,7 +4,11 @@ export default function TheBackgroundImage()
 {
     return (
         <div id="theBackground" className={style.backgroundStyle}>
-            <img src="./images/DYD-collection.jpg" alt="Foto di background" />
+            <img src="./images/DYD-collection.jpg" alt="Foto di background" className="absolute" />
+            <img src="./images/cartoons/cartoon1.jpg" alt="Vignetta nr 1" className="absolute top-20 right-72" />
+            <img src="./images/cartoons/cartoon2.jpg" alt="Vignetta nr 2" className="absolute top-0 left-0" />
+            <img src="./images/cartoons/cartoon3.jpg" alt="Vignetta nr 3" className="absolute -bottom-8 left-28" />
+            <img src="./images/cartoons/cartoon4.jpg" alt="Vignetta nr 4" className="absolute -bottom-24 right-0" />
         </div>
         );
 }
--- src/components/TheMainSection.jsx
+++ src/components/TheMainSection.jsx
@@ -4,7 +4,6 @@ export default function TheMainSection()
 {
     return (
         <main className={style.main}>
-
         </main>
     );
 }
