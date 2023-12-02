d33668e593c5e7d323bc6126124efaec4cb86e8f
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-12-02 12:54:41 +0100

    Collezione posizionata

--- src/components/TheMainSection.jsx
+++ src/components/TheMainSection.jsx
@@ -3,15 +3,15 @@ import albums from "../../public/files/titlesWithData.json";
 
 const percentageAndTWClasses =  {
                                     theMainSectionHeight    :   {
-                                                                    perc    :   80,
+                                                                    perc    :   0.8,
                                                                     TWClass :   "h-[80%]"
                                                                 },
                                     collectionWidth         :   {
-                                                                    perc    :   90,
+                                                                    perc    :   0.9,
                                                                     TWClass :   "w-[90%]"
                                                                 },
                                     controlsWidth           :   {
-                                                                    perc    :   3,
+                                                                    perc    :   0.03,
                                                                     TWClass :   "w-[3%]"
                                                                 } 
                                 }
@@ -20,7 +20,9 @@ export default function TheMainSection()
 {
     const totalAlbums = albums.length;
 
-    // const theMainSectionHeightPX = window.innerHeight * theMainSectionHeightPerc / 100;
+    const theMainSectionHeightPX = window.innerHeight * percentageAndTWClasses.theMainSectionHeight.perc;
+    const albumCollectionWidthPX = window.innerWidth * percentageAndTWClasses.collectionWidth.perc;
+    const sideAlbumWidthPX = (albumCollectionWidthPX - theMainSectionHeightPX) / (totalAlbums - 1);
 
     return (
         <main className={`${style.main} ${percentageAndTWClasses.theMainSectionHeight.TWClass}`}>
@@ -31,7 +33,10 @@ export default function TheMainSection()
                 {
                     albums.map( album =>
                         {
-
+                            return  (
+                                        <div key={album.nr} className={style.album} style={{width : `${sideAlbumWidthPX}px`}}>
+                                        </div>
+                                    );
                         })
                 }
                 <img src="./images/albums/DYD-40.jpg" alt="Vignetta nr 4" className={style.album} />
