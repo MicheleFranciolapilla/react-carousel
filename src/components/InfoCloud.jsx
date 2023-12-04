a6389b3ae26b66f20d41cc16b87c50300ee8c7c5
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-12-04 18:34:19 +0100

    Non ultimato.

--- src/components/InfoCloud.jsx
+++ src/components/InfoCloud.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import style from "../assets/style/modules/InfoCloudStyle.module.css";
 
 import VirtualInfoText from "./VirtualInfoText";
@@ -11,6 +12,7 @@ export default function InfoCloud({heading, info, headingColor, infoColor, bgCol
     let lowerBubbles = [];
     let leftBubbles = [];
     const maxRadius = 13;
+    const [appendFinished, setAppendFinished] = useState(false);
 
     function appendOnVirtual()
     {
@@ -28,55 +30,66 @@ export default function InfoCloud({heading, info, headingColor, infoColor, bgCol
                     console.log("RICERCA ULTIMATA: ", itemToVirtualize);
                     console.log("WIDTH: ", itemWidth);
                     console.log("HEIGHT: ", itemHeight);
+                    setBubblesCenters();
+                    setAppendFinished( (value) => value = true);
                 }
-            }, 300);
+            }, 200);
     }
 
     function setBubblesCenters()
     {
-        upperBubbles.push(0);
+        upperBubbles.push({center : 0, radius : Math.random() * maxRadius + 1});
         let beginU = 0;
         let endU = itemWidth;
+        console.log("PRIMA DEL WHILE........ BEGIN= ",beginU," - END= ",endU);
         while (beginU < endU)
         {
-            beginU += Math.ceil(Math.random() * (endU - beginU)); 
+            console.log("BEGIN: ", beginU);
+            console.log("END: ", endU);
+            beginU += Math.ceil(Math.random() * (maxRadius * 2 / 3)); 
             if (beginU < endU)
-                upperBubbles.push(beginU);
+            {
+                console.log("NUOVO BEGIN: ", beginU);
+                console.log("UPPERBUBBLES: ", upperBubbles);
+                upperBubbles.push({center : beginU, radius : Math.random() * maxRadius + 1});
+            }
         }
-        upperBubbles.push(endU);
+        upperBubbles.push({center : endU, radius : Math.random() * maxRadius + 1});
 
-        lowerBubbles.push(0);
+        console.log("UPPERBUBBLES: ", upperBubbles);
+
+        lowerBubbles.push({center : 0, radius : Math.random() * maxRadius + 1});
         let beginL = 0;
         let endL = itemWidth;
         while (beginL < endL)
         {
             beginL += Math.ceil(Math.random() * (endL - beginL)); 
             if (beginL < endL)
-                lowerBubbles.push(beginL);
+                lowerBubbles.push({center : beginL, radius : Math.random() * maxRadius + 1});
         }
-        lowerBubbles.push(endL);
+        lowerBubbles.push({center : endL, radius : Math.random() * maxRadius + 1});
 //************************************************************* */
-        rightBubbles.push(0);
+        rightBubbles.push({center : 0, radius : Math.random() * maxRadius + 1});
         let beginR = 0;
         let endR = itemHeight;
         while (beginR < endR)
         {
             beginR += Math.ceil(Math.random() * (endR - beginR)); 
             if (beginR < endR)
-                rightBubbles.push(beginR);
+                rightBubbles.push({center : beginR, radius : Math.random() * maxRadius + 1});
         }
-        rightBubbles.push(endR);
+        rightBubbles.push({center : endR, radius : Math.random() * maxRadius + 1});
 
-        leftBubbles.push(0);
+        leftBubbles.push({center : 0, radius : Math.random() * maxRadius + 1});
         let beginS = 0;
         let endS = itemHeight;
         while (beginS < endS)
         {
             beginS += Math.ceil(Math.random() * (endS - beginS)); 
             if (beginS < endS)
-                leftBubbles.push(beginS);
+                leftBubbles.push({center : beginS, radius : Math.random() * maxRadius + 1});
         }
-        leftBubbles.push(endS);
+        leftBubbles.push({center : endS, radius : Math.random() * maxRadius + 1});
     }
 
     return (
@@ -89,9 +102,23 @@ export default function InfoCloud({heading, info, headingColor, infoColor, bgCol
                 index={cloudIndex}
             />
             { appendOnVirtual() }
-            { setBubblesCenters() }
-
+            { appendFinished &&  
             <div id={`${cloudIndex}-${heading}-${info[0]}`} className={`${style.mainCloud} ${bgColor} relative`}>
+            {
+                upperBubbles.map( bubbleData =>
+                    <div 
+                        key={`upperbubble-${cloudIndex}-${heading}-${info[0]}-${bubbleData.center}-${bubbleData.radius}`}
+                        className={`${style.bubble} ${bgColor}`}
+                        style=
+                            {{
+                                width : bubbleData.radius * 2,
+                                aspectRatio : 1,
+                                top : -bubbleData.radius,
+                                left: bubbleData.center -bubbleData.radius
+                            }}
+                    >
+                    </div>)
+            }
             <span className={headingColor}>{heading}</span>
             { 
                 info.map( infoItem => 
@@ -102,7 +129,10 @@ export default function InfoCloud({heading, info, headingColor, infoColor, bgCol
                         {infoItem}
                     </span>) 
             }
-        </div>
+    </div>
+            }
+
+
         </>
 
     )
